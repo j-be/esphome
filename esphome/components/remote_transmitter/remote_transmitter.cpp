497f870cc39f98a3321051d0a725ccf521e791d0
Juri Berlanda <juriberlanda@hotmail.com>
2025-11-23 15:23:11 +0100

    RP2040: Add to existing remote_transmitter component

--- esphome/components/remote_transmitter/remote_transmitter.cpp
+++ esphome/components/remote_transmitter/remote_transmitter.cpp
@@ -2,7 +2,7 @@
 #include "esphome/core/log.h"
 #include "esphome/core/application.h"
 
-#if defined(USE_LIBRETINY) || defined(USE_ESP8266)
+#if defined(USE_LIBRETINY) || defined(USE_ESP8266) || defined(USE_RP2040)
 
 namespace esphome {
 namespace remote_transmitter {
